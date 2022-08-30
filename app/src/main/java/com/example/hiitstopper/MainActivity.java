807eb6e5016e68f85e08a7155fed160f0a7ababf
SirDNA <eperjesi.a@gmail.com>
2022-08-30 15:32:17 +0800

    - updated UI - added correction to timer (may need further correction) - removed some code repetition

--- app/src/main/java/com/example/hiitstopper/MainActivity.java
+++ app/src/main/java/com/example/hiitstopper/MainActivity.java
@@ -6,28 +6,40 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.NumberPicker;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
-// TODO 1: Implement a rest time timer
-// TODO 2: Modify timer to add sets
-// TODO 3: Replace vertical seekbars with circular seekbars
-// TODO 4: Add TextViews to circular seekbars
-// TODO 5: Add indication of progress on Timer view for user to know how many sets are left
+import com.google.android.material.button.MaterialButton;
+
+import me.tankery.lib.circularseekbar.CircularSeekBar;
+
+// COMPLETED 1: Implement a rest time timer
+// COMPLETED 2: Modify timer to add sets
+// COMPLETED 3: Replace vertical seekbars with circular seekbars
+// COMPLETED 4: Add Labels and TextViews to circular seekbars
+// COMPLETED 5: Added correction values to the timers for accuracy
+// COMPLETED 6: Updated font and style
+// COMPLETED 7: To remove code repetition, refactor setCountDownTime()
+//  so it returns a formatted String instead of setting text directly
+// TODO 8: Move methods setting the timer in a separate utility class
+// TODO 9: Add indication of progress on Timer view for user to know how many sets are left
 
 public class MainActivity extends AppCompatActivity {
 
     private int mNumberOfSets;
 
-    private SeekBar mSetsSeekBar;
-    private SeekBar mRestSeekBar;
-    private Button mStartTimerButton;
-    private Button mPauseTimerButton;
-    private Button mCancelTimerButton;
+    private CircularSeekBar mSetsSeekBar;
+    private CircularSeekBar mRestSeekBar;
+    private MaterialButton mStartTimerButton;
+    private MaterialButton mPauseTimerButton;
+    private MaterialButton mCancelTimerButton;
     private NumberPicker mSetPicker;
     private TextView mTimerText;
+    private TextView mLabelExercise;
+    private TextView mLabelRest;
+    private TextView mLabelSets;
+    private TextView mExerciseTime;
+    private TextView mRestTime;
 
     private CountDownTimer mActiveTimer;
     private long mMillisUntilExerciseFinished;
@@ -39,17 +51,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        mSetsSeekBar = (SeekBar) findViewById(R.id.seekbar_set);
-        mRestSeekBar = (SeekBar) findViewById(R.id.seekbar_rest);
-        mStartTimerButton = (Button) findViewById(R.id.button_start);
-        mPauseTimerButton = (Button) findViewById(R.id.button_pause);
-        mCancelTimerButton = (Button) findViewById(R.id.button_cancel);
+        mSetsSeekBar = (CircularSeekBar) findViewById(R.id.seekbar_set);
+        mRestSeekBar = (CircularSeekBar) findViewById(R.id.seekbar_rest);
+        mStartTimerButton = (MaterialButton) findViewById(R.id.button_start);
+        mPauseTimerButton = (MaterialButton) findViewById(R.id.button_pause);
+        mCancelTimerButton = (MaterialButton) findViewById(R.id.button_cancel);
         mSetPicker = (NumberPicker) findViewById(R.id.set_picker);
         mTimerText = (TextView) findViewById(R.id.tv_timer);
+        mLabelExercise = (TextView) findViewById(R.id.label_exercise);
+        mLabelRest = (TextView) findViewById(R.id.label_rest);
+        mLabelSets = (TextView) findViewById(R.id.label_sets);
+        mExerciseTime = (TextView) findViewById(R.id.tv_exercise_time);
+        mRestTime = (TextView) findViewById(R.id.tv_rest_time);
 
         mSetsSeekBar.setMax(180);
+        mSetsSeekBar.setProgress(60);
+        mExerciseTime.setText("1:00\u00A0");
         mRestSeekBar.setMax(90);
+        mRestSeekBar.setProgress(30);
+        mRestTime.setText("0:30\u00A0");
+
+        mSetsSeekBar.setOnSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(CircularSeekBar circularSeekBar, float v, boolean b) {
+
+                Float f = v * 1000;
+                //add long values of float v
+                mExerciseTime.setText(getFormattedCountDownTime(f.longValue()));
+            }
+
+            @Override
+            public void onStopTrackingTouch(CircularSeekBar circularSeekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(CircularSeekBar circularSeekBar) {
+
+            }
+        });
+
+        mRestSeekBar.setOnSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(CircularSeekBar circularSeekBar, float v, boolean b) {
+                Float f = v * 1000;
+                //add long values of float v
+                mRestTime.setText(getFormattedCountDownTime(f.longValue()));
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(CircularSeekBar circularSeekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(CircularSeekBar circularSeekBar) {
+
+            }
+        });
+
 
         mSetPicker.setDisplayedValues(getResources().getStringArray(R.array.sets));
         mSetPicker.setMinValue(1);
@@ -62,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 mNumberOfSets = mSetPicker.getValue();
                 Log.i("MainActivity", "The setsSeekbar progress value is " + mSetsSeekBar.getProgress());
-                startExerciseTimer(mSetsSeekBar.getProgress()*1000);
+                startExerciseTimer((int)mSetsSeekBar.getProgress()*1000);
             }
         });
         mPauseTimerButton.setOnClickListener(new View.OnClickListener() {
@@ -80,28 +141,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-    // TODO: There is a problem here. The timer often skips a second when it starts.
-    //  Lower the interval to 100 milliseconds and round it. This is as close you can get
-    //  to showing accurate seconds. Check stackoverflow for implementation examples
-
     private void startExerciseTimer(int exerciseTimeInMilliseconds){
 
+        int timeCorrectionMilliseconds = 499;
+        exerciseTimeInMilliseconds = timeCorrectionMilliseconds + exerciseTimeInMilliseconds;
+
         setTimerView();
-        setCurrentTime(exerciseTimeInMilliseconds);
+        mTimerText.setText(getFormattedCountDownTime(exerciseTimeInMilliseconds));
             mActiveTimer = new CountDownTimer(exerciseTimeInMilliseconds, 100) {
                 @Override
                 public void onTick(long millisUntilFinished) {
 
-                    mMillisUntilExerciseFinished = millisUntilFinished;
-                    setCurrentTime(mMillisUntilExerciseFinished);
+                    //TODO (optional): check if 0 is 1.5 seconds long. If it is, finish 499s earlier
+
+                    long roundedTime = Math.round((float) millisUntilFinished / 1000);
+                    mMillisUntilExerciseFinished = roundedTime * 1000;
+                    mTimerText.setText(getFormattedCountDownTime(mMillisUntilExerciseFinished));
                 }
 
                 @Override
                 public void onFinish() {
+                    mTimerText.setText(getFormattedCountDownTime(0));
                     mNumberOfSets --;
+                    try {
+                        wait(499);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     if (mNumberOfSets>0){
-                        startRestTimer(mRestSeekBar.getProgress()*1000);
+                        startRestTimer((int)mRestSeekBar.getProgress()*1000);
                     } else {
                         setSetupView();
                     }
@@ -126,12 +194,19 @@ public class MainActivity extends AppCompatActivity {
                 mActiveTimer = new CountDownTimer(mMillisUntilExerciseFinished, 100) {
                     @Override
                     public void onTick(long millisUntilFinished) {
-                        mMillisUntilExerciseFinished = millisUntilFinished;
-                        setCurrentTime(mMillisUntilExerciseFinished);
+                        long roundedTime = Math.round((float) millisUntilFinished / 1000);
+                        mMillisUntilRestFinished = roundedTime * 1000;
+                        mTimerText.setText(getFormattedCountDownTime(mMillisUntilRestFinished));
                     }
 
                     @Override
                     public void onFinish() {
+                        mTimerText.setText(getFormattedCountDownTime(0));
+                        try {
+                            wait(499);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         setSetupView();
                     }
                 }.start();
@@ -141,24 +216,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void startRestTimer(int restTimeInMilliseconds){
 
+        int timeCorrectionMilliseconds = 499;
+        restTimeInMilliseconds = timeCorrectionMilliseconds + restTimeInMilliseconds;
+
         setTimerView();
-        setCurrentTime(restTimeInMilliseconds);
+        mTimerText.setText(getFormattedCountDownTime(restTimeInMilliseconds));
 
         new CountDownTimer(restTimeInMilliseconds, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
-                mMillisUntilRestFinished = millisUntilFinished;
-                setCurrentTime(mMillisUntilRestFinished);
+                long roundedTime = Math.round((float) millisUntilFinished / 1000);
+                mMillisUntilRestFinished = roundedTime * 1000;
+                mTimerText.setText(getFormattedCountDownTime(mMillisUntilRestFinished));
             }
 
             @Override
             public void onFinish() {
-                startExerciseTimer(mSetsSeekBar.getProgress()*1000);
+                try {
+                    wait(499);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startExerciseTimer((int)mSetsSeekBar.getProgress()*1000);
             }
         }.start();
     }
 
     private void setTimerView() {
+        mExerciseTime.setVisibility(View.INVISIBLE);
+        mRestTime.setVisibility(View.INVISIBLE);
+        mLabelRest.setVisibility(View.INVISIBLE);
+        mLabelExercise.setVisibility(View.INVISIBLE);
+        mLabelSets.setVisibility(View.INVISIBLE);
         mStartTimerButton.setVisibility(View.INVISIBLE);
         mRestSeekBar.setVisibility(View.INVISIBLE);
         mSetsSeekBar.setVisibility(View.INVISIBLE);
@@ -169,6 +258,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setSetupView() {
+        mExerciseTime.setVisibility(View.VISIBLE);
+        mRestTime.setVisibility(View.VISIBLE);
+        mLabelRest.setVisibility(View.VISIBLE);
+        mLabelExercise.setVisibility(View.VISIBLE);
+        mLabelSets.setVisibility(View.VISIBLE);
         mRestSeekBar.setVisibility(View.VISIBLE);
         mSetsSeekBar.setVisibility(View.VISIBLE);
         mSetPicker.setVisibility(View.VISIBLE);
@@ -178,16 +272,16 @@ public class MainActivity extends AppCompatActivity {
         mPauseTimerButton.setVisibility(View.INVISIBLE);
     }
 
-    private void setCurrentTime(long millisUntilFinished) {
+    private String getFormattedCountDownTime(long millisUntilFinished) {
         int minutes = (int) millisUntilFinished / 1000 / 60;
-        int seconds = (int) millisUntilFinished /1000 % 60;
+        int seconds = (int) millisUntilFinished / 1000 % 60;
 
         String secondsWithZero = String.valueOf(seconds);
         if (seconds<10){
             secondsWithZero = "0" + seconds;
         }
 
-        String formattedTime = minutes + ":" + secondsWithZero;
-        mTimerText.setText(formattedTime);
+        String formattedTime = minutes + ":" + secondsWithZero + "\u00A0";
+        return formattedTime;
     }
 }
